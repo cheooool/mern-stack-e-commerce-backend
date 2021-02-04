889f54ce9601a9352c92054053c743105cc17d25
cheooool <g.cheooool@google.com>
2021-02-04 20:52:47 +0900

    mongoose 패키지 설치 및 db 연결과 product 샘플 코드 추가

--- app.js
+++ app.js
@@ -2,29 +2,70 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+require('dotenv/config');
+const api = process.env.API_URL;
 
 // middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-require('dotenv/config');
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
 
-const api = process.env.API_URL;
+const Product = mongoose.model('Product', productSchema);
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: 'hair dresser',
-    image: 'some_url',
-  };
-  res.send(product);
+app.get(`${api}/products`, async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList) {
+    res.status(500).json({
+      success: false,
+    });
+  }
+  res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.send(newProduct);
+  const { name, image, countInStock } = req.body;
+  const product = new Product({
+    name,
+    image,
+    countInStock,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
 });
+
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database',
+  })
+  .then(() => {
+    console.log('Database Connection is ready...');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(3000, () => {
   console.log(api);
